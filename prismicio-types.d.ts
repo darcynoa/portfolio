d5967dec03782fe72fd58014ce01f3452ba401a0
darcynoa <noahdarcy.dev@gmail.com>
2025-03-27 13:14:03 -0400

    Created showcase reusable page type

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -65,7 +65,72 @@ interface HomeDocumentData {
 export type HomeDocument<Lang extends string = string> =
   prismic.PrismicDocumentWithoutUID<Simplify<HomeDocumentData>, "home", Lang>;
 
-export type AllDocumentTypes = HomeDocument;
+type ShowcaseDocumentDataSlicesSlice = never;
+
+/**
+ * Content for Showcase documents
+ */
+interface ShowcaseDocumentData {
+  /**
+   * Slice Zone field in *Showcase*
+   *
+   * - **Field Type**: Slice Zone
+   * - **Placeholder**: *None*
+   * - **API ID Path**: showcase.slices[]
+   * - **Tab**: Main
+   * - **Documentation**: https://prismic.io/docs/field#slices
+   */
+  slices: prismic.SliceZone<ShowcaseDocumentDataSlicesSlice> /**
+   * Meta Title field in *Showcase*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: A title of the page used for social media and search engines
+   * - **API ID Path**: showcase.meta_title
+   * - **Tab**: SEO & Metadata
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */;
+  meta_title: prismic.KeyTextField;
+
+  /**
+   * Meta Description field in *Showcase*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: A brief summary of the page
+   * - **API ID Path**: showcase.meta_description
+   * - **Tab**: SEO & Metadata
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  meta_description: prismic.KeyTextField;
+
+  /**
+   * Meta Image field in *Showcase*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: showcase.meta_image
+   * - **Tab**: SEO & Metadata
+   * - **Documentation**: https://prismic.io/docs/field#image
+   */
+  meta_image: prismic.ImageField<never>;
+}
+
+/**
+ * Showcase document from Prismic
+ *
+ * - **API ID**: `showcase`
+ * - **Repeatable**: `true`
+ * - **Documentation**: https://prismic.io/docs/custom-types
+ *
+ * @typeParam Lang - Language API ID of the document.
+ */
+export type ShowcaseDocument<Lang extends string = string> =
+  prismic.PrismicDocumentWithUID<
+    Simplify<ShowcaseDocumentData>,
+    "showcase",
+    Lang
+  >;
+
+export type AllDocumentTypes = HomeDocument | ShowcaseDocument;
 
 /**
  * Default variation for Hero Slice
@@ -118,6 +183,9 @@ declare module "@prismicio/client" {
       HomeDocument,
       HomeDocumentData,
       HomeDocumentDataSlicesSlice,
+      ShowcaseDocument,
+      ShowcaseDocumentData,
+      ShowcaseDocumentDataSlicesSlice,
       AllDocumentTypes,
       HeroSlice,
       HeroSliceVariation,
